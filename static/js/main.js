0927d14a5206788e28daaf8b440b673d7951d18c
firas chiha <medfiraschiha@gmail.com>
2025-10-28 01:55:14 +0100

    merging with taher

--- static/js/main.js
+++ static/js/main.js
@@ -1,62 +1,68 @@
 /**
-* Template Name: Learner
-* Template URL: https://bootstrapmade.com/learner-bootstrap-course-template/
-* Updated: Jul 08 2025 with Bootstrap v5.3.7
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: Learner
+ * Template URL: https://bootstrapmade.com/learner-bootstrap-course-template/
+ * Updated: Jul 08 2025 with Bootstrap v5.3.7
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 
-(function() {
+(function () {
   "use strict";
 
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
   function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
+    const selectBody = document.querySelector("body");
+    const selectHeader = document.querySelector("#header");
     if (!selectHeader) return; // Check if header exists
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    if (
+      !selectHeader.classList.contains("scroll-up-sticky") &&
+      !selectHeader.classList.contains("sticky-top") &&
+      !selectHeader.classList.contains("fixed-top")
+    )
+      return;
+    window.scrollY > 100
+      ? selectBody.classList.add("scrolled")
+      : selectBody.classList.remove("scrolled");
   }
 
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
+  document.addEventListener("scroll", toggleScrolled);
+  window.addEventListener("load", toggleScrolled);
 
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
 
   function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
+    document.querySelector("body").classList.toggle("mobile-nav-active");
+    mobileNavToggleBtn.classList.toggle("bi-list");
+    mobileNavToggleBtn.classList.toggle("bi-x");
   }
   if (mobileNavToggleBtn) {
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+    mobileNavToggleBtn.addEventListener("click", mobileNavToogle);
   }
 
   /**
    * Hide mobile nav on same-page/hash links
    */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
+  document.querySelectorAll("#navmenu a").forEach((navmenu) => {
+    navmenu.addEventListener("click", () => {
+      if (document.querySelector(".mobile-nav-active")) {
         mobileNavToogle();
       }
     });
-
   });
 
   /**
    * Toggle mobile nav dropdowns
    */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
+  document.querySelectorAll(".navmenu .toggle-dropdown").forEach((navmenu) => {
+    navmenu.addEventListener("click", function (e) {
       e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      this.parentNode.classList.toggle("active");
+      this.parentNode.nextElementSibling.classList.toggle("dropdown-active");
       e.stopImmediatePropagation();
     });
   });
@@ -64,9 +70,9 @@
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
+  const preloader = document.querySelector("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       preloader.remove();
     });
   }
@@ -74,46 +80,48 @@
   /**
    * Scroll top button
    */
-  let scrollTop = document.querySelector('.scroll-top');
+  let scrollTop = document.querySelector(".scroll-top");
 
   function toggleScrollTop() {
     if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+      window.scrollY > 100
+        ? scrollTop.classList.add("active")
+        : scrollTop.classList.remove("active");
     }
   }
-  
+
   if (scrollTop) {
-    scrollTop.addEventListener('click', (e) => {
+    scrollTop.addEventListener("click", (e) => {
       e.preventDefault();
       window.scrollTo({
         top: 0,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     });
   }
 
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
+  window.addEventListener("load", toggleScrollTop);
+  document.addEventListener("scroll", toggleScrollTop);
 
   /**
    * Animation on scroll function and init
    */
   function aosInit() {
-    if (typeof AOS !== 'undefined') {
+    if (typeof AOS !== "undefined") {
       AOS.init({
         duration: 600,
-        easing: 'ease-in-out',
+        easing: "ease-in-out",
         once: true,
-        mirror: false
+        mirror: false,
       });
     }
   }
-  window.addEventListener('load', aosInit);
+  window.addEventListener("load", aosInit);
 
   /**
    * Initiate Pure Counter
    */
-  if (typeof PureCounter !== 'undefined') {
+  if (typeof PureCounter !== "undefined") {
     new PureCounter();
   }
 
@@ -121,18 +129,20 @@
    * Init swiper sliders
    */
   function initSwiper() {
-    if (typeof Swiper !== 'undefined') {
-      document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-        let config = JSON.parse(
-          swiperElement.querySelector(".swiper-config").innerHTML.trim()
-        );
+    if (typeof Swiper !== "undefined") {
+      document
+        .querySelectorAll(".init-swiper")
+        .forEach(function (swiperElement) {
+          let config = JSON.parse(
+            swiperElement.querySelector(".swiper-config").innerHTML.trim()
+          );
 
-        if (swiperElement.classList.contains("swiper-tab")) {
-          initSwiperWithCustomPagination(swiperElement, config);
-        } else {
-          new Swiper(swiperElement, config);
-        }
-      });
+          if (swiperElement.classList.contains("swiper-tab")) {
+            initSwiperWithCustomPagination(swiperElement, config);
+          } else {
+            new Swiper(swiperElement, config);
+          }
+        });
     }
   }
 
@@ -142,30 +152,33 @@
    * Pricing Toggle
    */
 
-  const pricingContainers = document.querySelectorAll('.pricing-toggle-container');
+  const pricingContainers = document.querySelectorAll(
+    ".pricing-toggle-container"
+  );
 
-  pricingContainers.forEach(function(container) {
-    const pricingSwitch = container.querySelector('.pricing-toggle input[type="checkbox"]');
-    const monthlyText = container.querySelector('.monthly');
-    const yearlyText = container.querySelector('.yearly');
+  pricingContainers.forEach(function (container) {
+    const pricingSwitch = container.querySelector(
+      '.pricing-toggle input[type="checkbox"]'
+    );
+    const monthlyText = container.querySelector(".monthly");
+    const yearlyText = container.querySelector(".yearly");
 
-    pricingSwitch.addEventListener('change', function() {
-      const pricingItems = container.querySelectorAll('.pricing-item');
+    pricingSwitch.addEventListener("change", function () {
+      const pricingItems = container.querySelectorAll(".pricing-item");
 
       if (this.checked) {
-        monthlyText.classList.remove('active');
-        yearlyText.classList.add('active');
-        pricingItems.forEach(item => {
-          item.classList.add('yearly-active');
+        monthlyText.classList.remove("active");
+        yearlyText.classList.add("active");
+        pricingItems.forEach((item) => {
+          item.classList.add("yearly-active");
         });
       } else {
-        monthlyText.classList.add('active');
-        yearlyText.classList.remove('active');
-        pricingItems.forEach(item => {
-          item.classList.remove('yearly-active');
+        monthlyText.classList.add("active");
+        yearlyText.classList.remove("active");
+        pricingItems.forEach((item) => {
+          item.classList.remove("yearly-active");
         });
       }
     });
   });
-
 })();
